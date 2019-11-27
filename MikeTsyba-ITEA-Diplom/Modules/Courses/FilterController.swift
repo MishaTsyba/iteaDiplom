9879fc22bca9ff7ba6f88d22a611cd8cb29013e5
MishaTsyba <mike.tsyba@gmail.com>
2019-11-27 22:22:04 +0200

    add linked Filter Outlet variables

--- MikeTsyba-ITEA-Diplom/Modules/Courses/FilterController.swift
+++ MikeTsyba-ITEA-Diplom/Modules/Courses/FilterController.swift
@@ -11,6 +11,69 @@ import UIKit
 class FilterController: UIViewController {
 
 	//MARK: - Interface Outlets
+	@IBOutlet weak var filterBackgroundView: UIView!
+	@IBOutlet weak var filterBackgroundImageView: UIImageView!
+
+	@IBOutlet weak var filterTitleView: UIView!
+	@IBOutlet weak var saveButtonView: UIView!
+	@IBOutlet weak var saveButton: UIButton!
+	@IBOutlet weak var backButtonView: UIView!
+	@IBOutlet weak var backButton: UIButton!
+	@IBOutlet weak var titleLabel: UILabel!
+
+	@IBOutlet weak var shadowFilterView: UIView!
+	@IBOutlet weak var filterContentView: UIView!
+
+	@IBOutlet weak var timeFilterView: UIView!
+	@IBOutlet weak var timeFilterLabel: UILabel!
+
+	@IBOutlet weak var dayFilterView: UIView!
+	@IBOutlet weak var dayCircleImageView: UIImageView!
+	@IBOutlet weak var dayFilterLabel: UILabel!
+	@IBOutlet weak var dayFilterButton: UIButton!
+
+	@IBOutlet weak var eveningFilterView: UIView!
+	@IBOutlet weak var eveningCircleImageView: UIImageView!
+	@IBOutlet weak var eveningFilterLabel: UILabel!
+	@IBOutlet weak var eveningFilterButton: UIButton!
+
+	@IBOutlet weak var typeFilterView: UIView!
+	@IBOutlet weak var typeFilterLabel: UILabel!
+
+	@IBOutlet weak var allTypesFilterView: UIView!
+	@IBOutlet weak var allTypesCircleImageView: UIImageView!
+	@IBOutlet weak var allTypesFilterLabel: UILabel!
+	@IBOutlet weak var allTypesFilterButton: UIButton!
+
+	@IBOutlet weak var mobDevFilterView: UIView!
+	@IBOutlet weak var mobDevCircleImageView: UIImageView!
+	@IBOutlet weak var mobDevFilterLabel: UILabel!
+	@IBOutlet weak var mobDevFilterButton: UIButton!
+
+	@IBOutlet weak var feDevFilterView: UIView!
+	@IBOutlet weak var feDevCircleImageView: UIImageView!
+	@IBOutlet weak var feDevFilterLabel: UILabel!
+	@IBOutlet weak var feDevFilterButton: UIButton!
+
+	@IBOutlet weak var designFilterView: UIView!
+	@IBOutlet weak var designCircleImageView: UIImageView!
+	@IBOutlet weak var designFilterLabel: UILabel!
+	@IBOutlet weak var designFilterButton: UIButton!
+
+	@IBOutlet weak var programmingFilterView: UIView!
+	@IBOutlet weak var programmingCircleImageView: UIImageView!
+	@IBOutlet weak var programmingFilterLabel: UILabel!
+	@IBOutlet weak var programmingFilterButton: UIButton!
+
+	@IBOutlet weak var qaFilterView: UIView!
+	@IBOutlet weak var qaCircleImageView: UIImageView!
+	@IBOutlet weak var qaFilterLabel: UILabel!
+	@IBOutlet weak var qaFilterButton: UIButton!
+
+	@IBOutlet weak var otherFilterView: UIView!
+	@IBOutlet weak var otherCircleImageView: UIImageView!
+	@IBOutlet weak var otherFilterLabel: UILabel!
+	@IBOutlet weak var otherFilterButton: UIButton!
 
 	//MARK: - Custom variables
 
